96c0a18c47995397d5b3800080d44180be27e41c
firespark <fireleit@gmail.com>
2024-09-28 00:55:40 +0800

    feat: add hash solutions

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
+import { createReadStream } from 'node:fs';
+import { createHash } from 'node:crypto';
+
+const filePath = './files/fileToCalculateHashFor.txt';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = createHash('sha256');
+    const stream = await createReadStream(filePath);
+
+    stream.on('data', (chunk) => {
+        hash.update(chunk);
+    });
+
+    stream.on('end', () => {
+        console.log(hash.digest('hex'));
+    });
+
+    stream.on('error', (err) => {
+        console.error(`Error reading file: ${err.message}`);
+    });
 };
 
 await calculateHash();
