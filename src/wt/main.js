b185e24d95d2f80c67d06c2317346ccd45fefee0
firespark <fireleit@gmail.com>
2024-09-29 23:08:19 +0800

    feat: add wt solutions

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,36 @@
+import { Worker } from 'worker_threads';
+import { cpus } from 'os';
+
 const performCalculations = async () => {
-    // Write your code here
+    const folderPath = import.meta.dirname;
+    const numCores = cpus().length;
+
+    let workers = [];
+
+    for (let i = 0; i < numCores; i++) {
+        const workerData = 10 + i;
+
+        workers.push(new Promise((resolve) => {
+
+            const worker = new Worker(`${folderPath}/worker.js`, {
+                workerData: workerData,
+            });
+
+            worker.on('message', (msg) => {
+                resolve({ status: 'resolved', data: msg });
+            });
+
+            worker.on('error', () => {
+                resolve({ status: 'error', data: null });
+            });
+
+        }));
+    }
+
+
+    const resultsArray = await Promise.all(workers);
+
+    console.log(resultsArray);
 };
 
 await performCalculations();
