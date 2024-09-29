52cf8e8910e4882ffb97a1f607074e59032c884b
firespark <fireleit@gmail.com>
2024-09-29 23:37:27 +0800

    feat: add cp solutions

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,24 @@
+import { spawn } from 'child_process';
+
+//const cliArgs = process.argv.slice(2);
+const cliArgs = ['arg1', 'arg2'];
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const folderPath = import.meta.dirname;
+    const filePath = `${folderPath}/files/script.js`;
+
+    const allArgs = [filePath, ...args];
+
+    const childProcess = spawn('node', allArgs);
+
+    process.stdin.on('data', (value) => {
+       childProcess.stdin.write(value);
+    });
+
+    childProcess.stdout.on('data', (value) => {
+        console.log(value.toString());
+    });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(cliArgs);
